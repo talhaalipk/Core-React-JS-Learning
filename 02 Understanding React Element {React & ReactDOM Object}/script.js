33d604a3933f4dab91187d738352433a1a052300
Talha Ali <[200~talhaali1234056@gmail.com>
2024-09-18 22:49:51 +0500

    Understanding element in React-JS

---
+++ 02 Understanding React Element {React & ReactDOM Object}/script.js
@@ -0,0 +1,59 @@
+/* First Element in react */
+
+// const h1 = React.createElement(
+//     "h1",
+//     { style: {width: "50%", margin: "auto" } },
+//     "My first React Element"
+// );
+
+// const root = ReactDOM.createRoot(document.querySelector("#root"));
+// root.render(h1);
+
+
+
+
+/* Creating caard using React.createElement() */
+
+const card = React.createElement(
+    "div",
+    {
+        id : "card",
+        style : {
+            width : "300px",
+            height : "300px" ,
+            borderRadius : "10px",
+            backgroundColor : "#d079f5",
+            display : "flex",
+            flexDirection : "column",
+            padding : "20px",
+            margin: "120px auto"
+        }
+    },
+    [
+        React.createElement("h1",
+            {
+                key : 1
+            },
+            "React.createElement()"
+        ),
+        React.createElement("p",
+            {
+                key : 2
+            },
+            "👋 Hello, I'm Talha Ali 🎓 Software Engineering Student @ Superior University 📚 Currently in my 4th Semester Happy coding! 🚀 👋 Hello, I'm Talha Ali 🎓 Software Engineering Student @ Superior University 📚 Currently in my 4th Semester Happy coding! 🚀  "
+        ),
+        React.createElement("button",
+            {
+                key : 3,
+                style : {
+                    margin : "0px auto",
+                    width : "40%"
+                }
+            },
+            "Contact Us"
+        )
+    ]
+);
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(card);
