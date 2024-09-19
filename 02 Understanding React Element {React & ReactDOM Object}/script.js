b41ead19ebceaa76404d4ec8d23d55eeff339c8c
Talha Ali <[200~talhaali1234056@gmail.com>
2024-09-19 19:56:42 +0500

    Understanding JSX and Babel in React-JS

--- 02 Understanding React Element {React & ReactDOM Object}/script.js
+++ 02 Understanding React Element {React & ReactDOM Object}/script.js
@@ -9,50 +9,50 @@
 // const root = ReactDOM.createRoot(document.querySelector("#root"));
 // root.render(h1);
 
-
-
-
 /* Creating caard using React.createElement() */
 
 const card = React.createElement(
-    "div",
-    {
-        id : "card",
-        style : {
-            width : "300px",
-            height : "300px" ,
-            borderRadius : "10px",
-            backgroundColor : "#d079f5",
-            display : "flex",
-            flexDirection : "column",
-            padding : "20px",
-            margin: "120px auto"
-        }
-    },
-    [
-        React.createElement("h1",
-            {
-                key : 1
-            },
-            "React.createElement()"
-        ),
-        React.createElement("p",
-            {
-                key : 2
-            },
-            "👋 Hello, I'm Talha Ali 🎓 Software Engineering Student @ Superior University 📚 Currently in my 4th Semester Happy coding! 🚀 👋 Hello, I'm Talha Ali 🎓 Software Engineering Student @ Superior University 📚 Currently in my 4th Semester Happy coding! 🚀  "
-        ),
-        React.createElement("button",
-            {
-                key : 3,
-                style : {
-                    margin : "0px auto",
-                    width : "40%"
-                }
-            },
-            "Contact Us"
-        )
-    ]
+	"div",
+	{
+		id: "card",
+		style: {
+			width: "300px",
+			height: "300px",
+			borderRadius: "10px",
+			backgroundColor: "#d079f5",
+			display: "flex",
+			flexDirection: "column",
+			padding: "20px",
+			margin: "120px auto",
+		},
+	},
+	[
+		React.createElement(
+			"h1",
+			{
+				key: 1,
+			},
+			"React.createElement()"
+		),
+		React.createElement(
+			"p",
+			{
+				key: 2,
+			},
+			"👋 Hello, I'm Talha Ali 🎓 Software Engineering Student @ Superior University 📚 Currently in my 4th Semester Happy coding! 🚀 👋 Hello, I'm Talha Ali 🎓 Software Engineering Student @ Superior University 📚 Currently in my 4th Semester Happy coding! 🚀  "
+		),
+		React.createElement(
+			"button",
+			{
+				key: 3,
+				style: {
+					margin: "0px auto",
+					width: "40%",
+				},
+			},
+			"Contact Us"
+		),
+	]
 );
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
---
+++ 03 JSX & Babel/script.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+const element = <div className="card" > </div>
+
+root.render(element);
+
